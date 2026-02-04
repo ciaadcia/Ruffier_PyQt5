7fee2137a2088d99465745faba6e5b3e968041dd
ciaa <154084369+ciaadcia@users.noreply.github.com>
2026-02-04 17:18:27 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,9 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (
-       QApplication, QWidget,
-       QHBoxLayout, QVBoxLayout,
-       QGroupBox, QRadioButton,
-       QPushButton, QLabel, QListWidget, QLineEdit)
+from PyQt5.QtWidgets import (QApplication, QWidget,QHBoxLayout, QVBoxLayout,QGroupBox, QRadioButton,QPushButton, QLabel, QListWidget, QLineEdit)
+
 from instr import *
 from final_win import *
 
@@ -68,4 +65,5 @@ class TestWin(QWidget):
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
+
         self.move(win_x, win_y)
