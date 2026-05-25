d50253b63467991f3788234a459894a8cbcfde72
ciaa <154084369+ciaadcia@users.noreply.github.com>
2026-05-25 20:11:32 +0700

    second_win.py def by hafidz

--- second_win.py
+++ second_win.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
 from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # checking the types of input values
-from PyQt5.QtWidgets import (
+from PyQt5.QtWidgets import ( 
        QApplication, QWidget,
        QHBoxLayout, QVBoxLayout, QGridLayout,
        QGroupBox, QRadioButton,
